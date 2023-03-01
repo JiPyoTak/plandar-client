a4019b465e1349c19090dfea6f9e0a2b43b7bb71
seokho10007 <sukho1007@naver.com>
2023-03-01 17:53:02 +0900

    chore: vite 절대경로 설정 및 기본포트 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
