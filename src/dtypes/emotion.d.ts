2ce5d145a0d8b4ebe923cc6248e49dc57d7fa4ba
seokho10007 <sukho1007@naver.com>
2023-03-01 17:52:26 +0900

    chore: 사용하지 않는 파일 제거

--- src/dtypes/emotion.d.ts
+++
@@ -1,8 +0,0 @@
-import { Theme as ThemeType } from '@emotion/react';
-
-declare module '@emotion/react' {
-  export interface Theme extends ThemeType {
-    primary: string;
-    background: string;
-  }
-}
