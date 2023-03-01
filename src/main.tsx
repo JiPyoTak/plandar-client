2ce99eeaa02f26440fc1545ac545ef06dfeda66f
seokho10007 <sukho1007@naver.com>
2023-03-01 17:55:30 +0900

    chore: react-query 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import App from '@/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
+const isProduction = process.env.NODE_ENV === 'production';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={!isProduction} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
