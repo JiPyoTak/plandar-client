c9a92d0a68d5d9f2a21d5aae9fac1e8ff2b1537e
pyo-sh <pygosky@gmail.com>
2023-03-03 14:26:30 +0900

    feat: 색상 결정 및 Emotion Theme에 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from '@/App';
+import { theme } from '@/theme/theme';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 const queryClient = new QueryClient();
-
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={!isProduction} />
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={!isProduction} />
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,38 @@
 import { Theme } from '@emotion/react';
 
-export const lightTheme: Theme = {
-  primary: '#000000',
-  background: '#ffffff',
+export const theme: Theme = {
+  primary: '#52D681',
+  primary_light: '#B5FF7D',
+  primary_dark: '#00AD7C',
+  point: '#FFF8B5',
+  placeholder: '#F8F8F8',
+  border1: '#F3F3F3',
+  border2: '#DEDEDE',
+  border3: '#ADADAD',
+  background1: '#FFFFFF',
+  background2: '#F6F6F6',
+  background3: '#E9E9E9',
+  background4: '#E2E2E2',
+  title_active: '#222222',
+  text1: '#313131',
+  text2: '#454545',
+  text3: '#868686',
+  text4: '#CECECE',
+  red: '#FB949B',
+  red_light: '#FFD4D6',
+  red_dark: '#F66570',
+  blue: '#97C5FF',
+  blue_light: '#C9E2FF',
+  blue_dark: '#4593FC',
+  orange: '#FFC96F',
+  orange_light: '#FFE0B2',
+  orange_dark: '#FE9C08',
+  emerald: '#BCEAD5',
+  emerald_light: '#DEF5E5',
+  emerald_dark1: '#9ED5C5',
+  emerald_dark2: '#8EC3B0',
+  white: '#FFFFFF',
+  black: '#000000',
 };
 
-export const darkTheme: Theme = {
-  primary: '#000000',
-  background: '#ffffff',
-};
-
-export type ThemeType = typeof lightTheme;
+export type ThemeType = typeof theme;
