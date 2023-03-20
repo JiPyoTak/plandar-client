f1f77f5c80b5a78e814e73a48786d5db371ad022
pyo-sh <pygosky@gmail.com>
2023-03-20 17:10:47 +0900

    feat: 전체 스크롤에 대해 스타일 설정 (#4)

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,36 +1,51 @@
-import { Global, css } from '@emotion/react';
+import { Global, css, useTheme } from '@emotion/react';
 
 import { FONT_INITILIZE } from './font';
 import { reset } from '@/styles/reset';
 
-const GlobalStyle: React.FC = () => (
-  <Global
-    styles={css`
-      ${reset}
-      :root {
-        ${FONT_INITILIZE}
-      }
-      * {
-        box-sizing: border-box;
-      }
+const GlobalStyle: React.FC = () => {
+  const theme = useTheme();
 
-      html {
-        font-size: 30px;
-      }
+  return (
+    <Global
+      styles={css`
+        ${reset}
+        :root {
+          ${FONT_INITILIZE}
+        }
+        * {
+          box-sizing: border-box;
+        }
 
-      body {
-        width: 100vw;
-        height: 100vh;
+        html {
+          font-size: 30px;
+        }
 
-        overflow: hidden;
-      }
+        body {
+          width: 100vw;
+          height: 100vh;
 
-      #root {
-        width: 100%;
-        height: 100%;
-      }
-    `}
-  />
-);
+          overflow: hidden;
+        }
+
+        #root {
+          width: 100%;
+          height: 100%;
+        }
+
+        body::-webkit-scrollbar {
+          width: 5px;
+          background-color: transparent;
+        }
+        body::-webkit-scrollbar-thumb {
+          background-clip: padding-box;
+          border-radius: 2px;
+          border: none;
+          background-color: ${theme.placeholder};
+        }
+      `}
+    />
+  );
+};
 
 export default GlobalStyle;
