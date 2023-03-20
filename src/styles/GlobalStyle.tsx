a425d71414506d64b756db7d704e8a785b5aaa13
pyo-sh <pygosky@gmail.com>
2023-03-20 17:09:45 +0900

    fix: feat: #root 화면 채우기를 body로 채우기 (#4)  - scroll 설정이 계층적이여야하기 때문이다

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,7 +3,7 @@ import { Global, css } from '@emotion/react';
 import { FONT_INITILIZE } from './font';
 import { reset } from '@/styles/reset';
 
-const GlobalStyle = () => (
+const GlobalStyle: React.FC = () => (
   <Global
     styles={css`
       ${reset}
@@ -18,9 +18,16 @@ const GlobalStyle = () => (
         font-size: 30px;
       }
 
-      #root {
+      body {
         width: 100vw;
         height: 100vh;
+
+        overflow: hidden;
+      }
+
+      #root {
+        width: 100%;
+        height: 100%;
       }
     `}
   />
