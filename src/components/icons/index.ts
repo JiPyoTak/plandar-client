7646915254c85f7d52b4cd2e337af2973ae60591
pyo-sh <pygosky@gmail.com>
2023-03-27 04:19:53 +0900

    feat: Plus Icon 추가 (#3)

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,13 +1,17 @@
 import { ReactComponent as CheckIconComponent } from '@/assets/check-icon.svg';
 import { ReactComponent as PencilIconComponent } from '@/assets/pencil-icon.svg';
+import { ReactComponent as PlusIconComponent } from '@/assets/plus-icon.svg';
 import frameIconComponent from '@/utils/frameIconComponent';
 
 const ICON_COMPONENTS = {
   CheckIcon: CheckIconComponent,
   PencilIcon: PencilIconComponent,
+  PlusIcon: PlusIconComponent,
 } as const;
 
-export const { CheckIcon, PencilIcon } = Object.entries(ICON_COMPONENTS).reduce(
+export const { CheckIcon, PencilIcon, PlusIcon } = Object.entries(
+  ICON_COMPONENTS,
+).reduce(
   (result, [key, IconComponent]) => ({
     ...result,
     [key]: frameIconComponent(IconComponent),
