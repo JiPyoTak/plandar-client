e532a2650f673ac36470c9c0573463ba8fa77a82
seokho10007 <sukho1007@naver.com>
2023-04-03 14:35:11 +0900

    feat: 컴포넌트 내부 state를 통한 일자 변경 (#2) - 작은 달력에서 월을 변경할 경우 전역 state는 변경되지 않도록 구션 - 일자를 변경할 경우 전역 state에 변경된 일자를 저장하도록 구현

--- src/components/Calendar/small/index.tsx
+++ src/components/Calendar/small/index.tsx
@@ -1,57 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
+import CalendarBody from './CalendarBody';
 import CalendarHeader from './CalendarHeader';
-import BaseCalendar from '@/components/common/calendar';
+
+import DayOfWeek from '@/components/common/calendar/DayOfWeek';
 import useDateStore from '@/stores/date';
-import { theme } from '@/styles/theme';
-
-// 작은 달력
-// - 월/주: 일자 클릭시 해당 날짜가 현재 월에 포함된 날짜인지 검사 후 포함되지 않으면 이전/다음 월로 이동
-// - 주: 일자 클릭시 해당 날짜가 현재 주에 포함된 날짜인지 검사 후 포함된다면 해당 주차를 달력에 표시
-// - 일: 일자 클릭시 선택된 날짜로 이동
-
-const ALL_DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+import { decrease, increase } from '@/utils/monthHandler';
 
 const Calendar = () => {
   const dateState = useDateStore();
-  const {
-    year,
-    month,
-    day,
-    increaseMonth,
-    decreaseMonth,
-    onChangeDate: onChangeDay,
-  } = dateState;
+  const { onChangeDate: onChangeDay, year, month, day } = dateState;
+
+  const [date, setDate] = useState({ year, month, day });
+
+  useEffect(() => {
+    setDate({ year, month, day });
+  }, [year, month, day]);
+
+  const increaseMonth = () => {
+    setDate(increase);
+  };
+
+  const decreaseMonth = () => {
+    setDate(decrease);
+  };
 
   return (
     <Container>
       <CalendarHeader
-        year={year}
-        month={month}
+        year={date.year}
+        month={date.month}
         increaseMonth={increaseMonth}
         decreaseMonth={decreaseMonth}
       />
-      <AllDay>
-        {ALL_DAYS.map((day) => (
-          <div key={day} css={(day === '일' || day === '토') && Color}>
-            {day}
-          </div>
-        ))}
-      </AllDay>
-      <BaseCalendar
-        date={{ year, month, day }}
-        onClickDayNumber={onChangeDay}
-      />
+      <DayOfWeek />
+      <CalendarBody date={date} onChangeDay={onChangeDay} />
     </Container>
   );
 };
-
-const Color = css`
-  color: ${theme.text3};
-`;
 
 const Container = styled.div`
   display: flex;
@@ -60,20 +48,6 @@ const Container = styled.div`
   gap: 0.5rem;
   font-size: 0.8rem;
   padding: 0 2rem 0 2rem;
-`;
-
-const AllDay = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  align-items: stretch;
-
-  & > div {
-    height: 18px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
 `;
 
 export default Calendar;
