9b9301fd380963286c64da1240bedda7c3737aac
jintak0401 <jintak0401@naver.com>
2023-04-04 14:53:14 +0900

    feat: storybook에서 svg 사용할 수 있도록 설정 추가 (#8)

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,13 +7,19 @@ module.exports = {
   core: {
     builder: '@storybook/builder-vite',
   },
+
   async viteFinal(config) {
     const { config: userConfig } = await loadConfigFromFile(
       path.resolve(__dirname, '../vite.config.ts'),
     );
+
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react')),
+    );
+
     return mergeConfig(config, {
       ...userConfig,
-      plugins: [],
     });
   },
 };
