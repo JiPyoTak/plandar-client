6e9eb08a29f0df05202de1e3aad347ad3f9d0ccd
jintak0401 <jintak0401@naver.com>
2023-04-04 16:17:57 +0900

    feat: StylishButton에 Storybook 변경 (#8)
    - Colored: 색상이 적용된 버튼
    - NonColor: 색상이 없는 버튼

--- src/stories/buttons/StylishButton.stories.tsx
+++ src/stories/buttons/StylishButton.stories.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-import { css } from '@emotion/react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import StylishButton from '@/components/buttons/StylishButton';
-import { PlusIcon } from '@/components/icons';
 
 export default {
   title: 'Buttons/StylishButton',
@@ -15,6 +13,7 @@ export default {
     size: { control: 'text' },
     color: { control: 'boolean' },
     square: { control: 'boolean' },
+    radiusDir: { control: 'text' },
   },
 } as ComponentMeta<typeof StylishButton>;
 
@@ -22,8 +21,8 @@ const Template: ComponentStory<typeof StylishButton> = (args) => (
   <StylishButton {...args} />
 );
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Colored = Template.bind({});
+Colored.args = {
   onClick: () => console.log('clicked'),
   color: true,
   children: 'Hello World',
@@ -34,28 +33,4 @@ NonColor.args = {
   onClick: () => console.log('clicked'),
   color: false,
   children: 'Hello World',
-};
-
-export const PlusButton = Template.bind({});
-PlusButton.args = {
-  onClick: () => console.log('clicked'),
-  size: 'small',
-  color: true,
-
-  children: (
-    <PlusIcon
-      css={css`
-        width: 1.5rem;
-        height: 1.5rem;
-      `}
-      color="white"
-    />
-  ),
-};
-
-export const TodayButton = Template.bind({});
-TodayButton.args = {
-  onClick: () => console.log('clicked'),
-  size: 'small',
-  children: '오늘',
 };
