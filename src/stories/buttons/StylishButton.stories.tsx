39e46a402646ba6a945fe7e482188234d491ae33
jintak0401 <jintak0401@naver.com>
2023-04-04 14:53:54 +0900

    feat: StylishButton storybook 구현 (#8)

--- src/stories/buttons/StylishButton.stories.tsx
+++ src/stories/buttons/StylishButton.stories.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { css } from '@emotion/react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import StylishButton from '@/components/buttons/StylishButton';
+import { PlusIcon } from '@/components/icons';
 
 export default {
   title: 'Buttons/StylishButton',
@@ -10,6 +12,9 @@ export default {
   argTypes: {
     onClick: { control: 'function' },
     children: { control: 'text' },
+    size: { control: 'text' },
+    color: { control: 'boolean' },
+    square: { control: 'boolean' },
   },
 } as ComponentMeta<typeof StylishButton>;
 
@@ -20,5 +25,37 @@ const Template: ComponentStory<typeof StylishButton> = (args) => (
 export const Primary = Template.bind({});
 Primary.args = {
   onClick: () => console.log('clicked'),
+  color: true,
   children: 'Hello World',
+};
+
+export const NonColor = Template.bind({});
+NonColor.args = {
+  onClick: () => console.log('clicked'),
+  color: false,
+  children: 'Hello World',
+};
+
+export const PlusButton = Template.bind({});
+PlusButton.args = {
+  onClick: () => console.log('clicked'),
+  size: 'small',
+  color: true,
+
+  children: (
+    <PlusIcon
+      css={css`
+        width: 1.5rem;
+        height: 1.5rem;
+      `}
+      color="white"
+    />
+  ),
+};
+
+export const TodayButton = Template.bind({});
+TodayButton.args = {
+  onClick: () => console.log('clicked'),
+  size: 'small',
+  children: '오늘',
 };
