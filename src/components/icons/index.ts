ed1a1f1fd35598737244758d73a54386be946d0e
pyo-sh <pygosky@gmail.com>
2023-04-06 15:41:27 +0900

    feat: Google Icon 추가 및 icon Component 선언 (#16)

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,5 +1,6 @@
 import { ReactComponent as CheckIconComponent } from '@/assets/check-icon.svg';
 import { ReactComponent as CrossIconComponent } from '@/assets/cross-icon.svg';
+import { ReactComponent as GoogleIconComponent } from '@/assets/google-icon.svg';
 import { ReactComponent as KakaoIconComponent } from '@/assets/kakao-icon.svg';
 import { ReactComponent as PencilIconComponent } from '@/assets/pencil-icon.svg';
 import { ReactComponent as PlusIconComponent } from '@/assets/plus-icon.svg';
@@ -8,17 +9,24 @@ import frameIconComponent from '@/utils/frameIconComponent';
 const ICON_COMPONENTS = {
   CheckIcon: CheckIconComponent,
   CrossIcon: CrossIconComponent,
+  GoogleIcon: GoogleIconComponent,
   KakaoIcon: KakaoIconComponent,
   PencilIcon: PencilIconComponent,
   PlusIcon: PlusIconComponent,
 } as const;
 
-export const { CheckIcon, CrossIcon, KakaoIcon, PencilIcon, PlusIcon } =
-  Object.entries(ICON_COMPONENTS).reduce(
-    (result, [key, IconComponent]) => ({
-      ...result,
-      [key]: frameIconComponent(IconComponent),
-    }),
-    {} as typeof ICON_COMPONENTS,
-  );
+export const {
+  CheckIcon,
+  CrossIcon,
+  GoogleIcon,
+  KakaoIcon,
+  PencilIcon,
+  PlusIcon,
+} = Object.entries(ICON_COMPONENTS).reduce(
+  (result, [key, IconComponent]) => ({
+    ...result,
+    [key]: frameIconComponent(IconComponent),
+  }),
+  {} as typeof ICON_COMPONENTS,
+);
 export * as ChevronIcon from './ChevronIcon';
