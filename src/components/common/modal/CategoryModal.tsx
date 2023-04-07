62a57f491b3fc5944a1e726187df644e42812d4c
jintak0401 <jintak0401@naver.com>
2023-04-07 17:23:00 +0900

    feat: 카테고리 생성 모달 임시 구현 (#15)

---
+++ src/components/common/modal/CategoryModal.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import ModalContainer from './index';
+import ColorPicker from '@/components/common/dropdown/ColorPicker';
+import { PencilIcon } from '@/components/icons';
+import { TSelectableColor } from '@/types';
+
+const CategoryModal = () => {
+  const theme = useTheme();
+  const [selectedColor, setSelectedColor] =
+    useState<TSelectableColor>('primary');
+  return (
+    <Modal
+      isBgBlack={true}
+      HeaderLeftComponent={
+        <ColorPicker
+          selectedColor={selectedColor}
+          onSelect={(color: TSelectableColor) => null}
+        />
+      }
+      HeaderRightComponent={
+        <button>
+          <PencilIcon color={'black'} />
+        </button>
+      }
+    >
+      바디입니다
+    </Modal>
+  );
+};
+
+const Modal = styled(ModalContainer)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 214px;
+  padding: 24px;
+  box-shadow: 1px 10px 25px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+`;
+
+export default CategoryModal;
