ceb37c50134d316b828726b838b20f40e3636ccc
jintak0401 <jintak0401@naver.com>
2023-04-07 17:22:06 +0900

    feat: 최상위 Modal 컴포넌트 구현 (#15)

---
+++ src/components/common/modal/index.tsx
@@ -0,0 +1,86 @@
+import React, { PropsWithChildren } from 'react';
+import { createPortal } from 'react-dom';
+
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { CrossIcon } from '@/components/icons';
+
+type TModalProps = PropsWithChildren<{
+  onClose?: (args: any) => any;
+  isBgBlack?: boolean;
+  HeaderLeftComponent?: React.ReactNode;
+  HeaderRightComponent?: React.ReactNode;
+  className?: string;
+}>;
+
+type TModalPortal = React.FC<TModalProps>;
+
+const Modal: TModalPortal = ({
+  children,
+  HeaderLeftComponent,
+  HeaderRightComponent,
+  onClose = () => null,
+  isBgBlack = false,
+  className,
+}: TModalProps) => {
+  const theme = useTheme();
+  const modalElement = document.getElementById('modal');
+  if (!modalElement) {
+    throw new Error('모달창을 열 수 없습니다');
+  }
+  return createPortal(
+    <Container
+      css={
+        isBgBlack && {
+          backgroundColor: theme.black,
+          opacity: 0.7,
+        }
+      }
+    >
+      <Body className={className}>
+        <Header>
+          <HeaderLeft>{HeaderLeftComponent}</HeaderLeft>
+          <HeaderRight>
+            {HeaderRightComponent}
+            <button onClick={onClose}>
+              <CrossIcon color={theme.black} />
+            </button>
+          </HeaderRight>
+        </Header>
+        {children}
+      </Body>
+    </Container>,
+    modalElement,
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.white};
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderComponent = styled.span`
+  display: flex;
+  gap: 8px;
+`;
+
+const HeaderLeft = styled(HeaderComponent)`
+  justify-content: flex-end;
+`;
+
+const HeaderRight = styled(HeaderComponent)`
+  justify-content: flex-start;
+`;
+export default Modal;
