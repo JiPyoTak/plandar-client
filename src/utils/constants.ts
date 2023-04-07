e0439d478eef92ea1218b66649970cb2bc3da170
jintak0401 <jintak0401@naver.com>
2023-04-07 17:21:20 +0900

    feat: 선택 가능한 색상이름 배열로 정의 (#15)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,17 @@ const CALENDAR_UNIT = ['일', '주', '월'] as const;
 
 const DAY_OF_WEEK_UNIT = ['일', '월', '화', '수', '목', '금', '토'] as const;
 
-export { CALENDAR_UNIT, DAY_OF_WEEK_UNIT };
+const SELECTABLE_COLOR = [
+  'primary',
+  'primary_light',
+  'red',
+  'red_light',
+  'orange',
+  'orange_light',
+  'emerald',
+  'emerald_light',
+  'blue',
+  'blue_light',
+];
+
+export { CALENDAR_UNIT, DAY_OF_WEEK_UNIT, SELECTABLE_COLOR };
