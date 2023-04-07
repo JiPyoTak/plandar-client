51ed458da6d8b4491ff67f8df1cf926ba4c64945
seokho10007 <sukho1007@naver.com>
2023-04-07 14:51:45 +0900

    feat: 조건에 따른 class name 추가 및 스타일링 (#2, #12)

--- src/components/common/calendar/DayOfWeek.tsx
+++ src/components/common/calendar/DayOfWeek.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
+import { TDayOfWeekUnit } from '@/types';
 import { DAY_OF_WEEK_UNIT } from '@/utils/constants';
 
 interface IProps {
   className?: string;
 }
 
+const DAY_OF_WEEK: {
+  [key in TDayOfWeekUnit]?: string;
+} = {
+  일: 'sunday',
+  토: 'saturday',
+};
+
 const DayOfWeek: React.FC<IProps> = ({ className }) => {
   return (
     <Container className={className}>
       {DAY_OF_WEEK_UNIT.map((day) => (
-        <div
-          key={day}
-          className={day === '토' || day === '일' ? 'weekend' : ''}
-        >
+        <div key={day} className={DAY_OF_WEEK[day] ?? ''}>
           {day}
         </div>
       ))}
@@ -28,9 +33,14 @@ const Container = styled.div`
   grid-template-columns: repeat(7, 1fr);
   align-items: stretch;
 
-  & > .weekend {
-    color: ${({ theme }) => theme.text3};
+  & > .sunday {
+    color: ${({ theme }) => theme.red};
   }
+
+  & > .saturday {
+    color: ${({ theme }) => theme.blue_dark};
+  }
+
   & > div {
     display: flex;
     align-items: center;
