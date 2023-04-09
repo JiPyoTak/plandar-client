4df99722df89544e28b7c671f5b0bf915b844c25
pyo-sh <pygosky@gmail.com>
2023-04-09 11:53:12 +0900

    feat: Client에서 사용하는 환경변수 추가 (#16)  - 서버 URL  - Access token key

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,7 @@ const CALENDAR_UNIT = ['일', '주', '월'] as const;
 
 const DAY_OF_WEEK_UNIT = ['일', '월', '화', '수', '목', '금', '토'] as const;
 
-export { CALENDAR_UNIT, DAY_OF_WEEK_UNIT };
+const SERVER_URL = import.meta.env.VITE_APP_SERVER_URL;
+const ACCESS_TOKEN_KEY = import.meta.env.VITE_APP_ACCESS_TOKEN_KEY;
+
+export { CALENDAR_UNIT, DAY_OF_WEEK_UNIT, SERVER_URL, ACCESS_TOKEN_KEY };
