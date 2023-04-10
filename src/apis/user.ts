b1db7328a2ac52b323a5cb0a0f9e32d36fcd215b
pyo-sh <pygosky@gmail.com>
2023-04-10 15:32:41 +0900

    feat: Logout API 선언 (#16)

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,9 +1,20 @@
+import axios from 'axios';
+
 import { axiosAPI } from '@/apis/axios';
 import { IUser } from '@/stores/user';
+import { SERVER_URL } from '@/utils/constants';
 
 const getUserAPI = async (): Promise<{ success: boolean; data: IUser }> => {
   const { data } = await axiosAPI.get(`/user`);
   return data;
 };
 
-export { getUserAPI };
+const logoutAPI = () => {
+  return axios.post(
+    `/auth/logout`,
+    {},
+    { baseURL: SERVER_URL, withCredentials: true },
+  );
+};
+
+export { getUserAPI, logoutAPI };
