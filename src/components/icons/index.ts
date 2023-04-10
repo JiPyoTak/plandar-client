53524b5ca8f4443f11c8f211fdaa407d9c17078b
jintak0401 <jintak0401@naver.com>
2023-04-10 16:25:19 +0900

    feat: Search 아이콘 추가 (#15)

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,6 +2,7 @@ import { ReactComponent as CheckIconComponent } from '@/assets/check-icon.svg';
 import { ReactComponent as CrossIconComponent } from '@/assets/cross-icon.svg';
 import { ReactComponent as PencilIconComponent } from '@/assets/pencil-icon.svg';
 import { ReactComponent as PlusIconComponent } from '@/assets/plus-icon.svg';
+import { ReactComponent as SearchIconComponent } from '@/assets/search-icon.svg';
 import frameIconComponent from '@/utils/frameIconComponent';
 
 const ICON_COMPONENTS = {
@@ -9,15 +10,15 @@ const ICON_COMPONENTS = {
   PencilIcon: PencilIconComponent,
   PlusIcon: PlusIconComponent,
   CrossIcon: CrossIconComponent,
+  SearchIcon: SearchIconComponent,
 } as const;
 
-export const { CheckIcon, PencilIcon, PlusIcon, CrossIcon } = Object.entries(
-  ICON_COMPONENTS,
-).reduce(
-  (result, [key, IconComponent]) => ({
-    ...result,
-    [key]: frameIconComponent(IconComponent),
-  }),
-  {} as typeof ICON_COMPONENTS,
-);
+export const { CheckIcon, PencilIcon, PlusIcon, CrossIcon, SearchIcon } =
+  Object.entries(ICON_COMPONENTS).reduce(
+    (result, [key, IconComponent]) => ({
+      ...result,
+      [key]: frameIconComponent(IconComponent),
+    }),
+    {} as typeof ICON_COMPONENTS,
+  );
 export * as ChevronIcon from './ChevronIcon';
