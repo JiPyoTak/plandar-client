d3f89bd783a6d3d07f787779b7e847f3f624f829
jintak0401 <jintak0401@naver.com>
2023-04-10 14:07:39 +0900

    feat: ColorPicker에 사용할 색상 변경 (#15)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,18 +1,20 @@
+import { theme } from '@/styles/theme';
+
 const CALENDAR_UNIT = ['일', '주', '월'] as const;
 
 const DAY_OF_WEEK_UNIT = ['일', '월', '화', '수', '목', '금', '토'] as const;
 
-const SELECTABLE_COLOR = [
+const SELECTABLE_COLOR: ReadonlyArray<keyof typeof theme> = [
   'primary',
-  'primary_light',
+  'primary_dark',
   'red',
-  'red_light',
+  'red_dark',
   'orange',
-  'orange_light',
-  'emerald',
-  'emerald_light',
+  'orange_dark',
+  'emerald_dark1',
+  'emerald_dark2',
   'blue',
-  'blue_light',
-];
+  'blue_dark',
+] as const;
 
 export { CALENDAR_UNIT, DAY_OF_WEEK_UNIT, SELECTABLE_COLOR };
