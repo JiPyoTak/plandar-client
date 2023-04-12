58ca4eadb1c4d49f8fd1c9795816beee0fe52025
pyo-sh <pygosky@gmail.com>
2023-04-12 16:48:18 +0900

    feat: Timetable의 기본 틀 스타일 적용 (#20)

---
+++ src/components/timetable/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+import moment from 'moment';
+
+import useDateState from '@/stores/date';
+
+type TProps = {
+  rangeAmount: number;
+};
+
+const Timetable: React.FC<TProps> = ({ rangeAmount }) => {
+  const { year, month, day } = useDateState();
+
+  const range = Math.min(rangeAmount, 7);
+  const stateMoment = moment({ year, month: month - 1, day });
+  const dateMoments = Array.from(Array(range), (_, index) =>
+    stateMoment.clone().add(index, 'days'),
+  );
+
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export default Timetable;
