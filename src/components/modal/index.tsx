018a77630da03cd423eed78476671b5036f55423
jintak0401 <jintak0401@naver.com>
2023-04-12 13:49:17 +0900

    feat: modal 배경에 position: fixed 속성 추가 (#15)

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -50,12 +50,14 @@ const Modal: TModalPortal = ({
 };
 
 const Container = styled.div`
-  position: relative;
   width: 100vw;
   height: 100vh;
 `;
 
 const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100vw;
   height: 100vh;
   background-color: ${({ theme }) => theme.black};
