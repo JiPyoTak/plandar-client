0e6bca85b32c89daf4efb41d4248533b16a4b74e
jintak0401 <jintak0401@naver.com>
2023-04-12 15:27:02 +0900

    feat: ICategory[]에 맞게 category mocking 데이터 수정 (#15)

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,6 +1,7 @@
-import { TSelectableColor } from '@/types';
+import { ICategory } from '@/types/rq/category';
 
-const CATEGORY_MOCK: { id: number; title: string; color: TSelectableColor }[] =
-  [{ id: 1, title: '테스트1', color: 'primary' }];
+const CATEGORY_MOCK: ICategory[] = [
+  { id: 1, name: '테스트1', color: '#52d681' },
+];
 
 export { CATEGORY_MOCK };
