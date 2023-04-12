4772c59af521d92333c22239e8d171d5d8133799
jintak0401 <jintak0401@naver.com>
2023-04-12 13:48:46 +0900

    feat: 카테고리 mocking 데이터 추가 (#15)

---
+++ src/utils/mock.ts
@@ -0,0 +1,6 @@
+import { TSelectableColor } from '@/types';
+
+const CATEGORY_MOCK: { id: number; title: string; color: TSelectableColor }[] =
+  [{ id: 1, title: '테스트1', color: 'primary' }];
+
+export { CATEGORY_MOCK };
