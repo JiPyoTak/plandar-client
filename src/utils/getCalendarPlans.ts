64884f37e089c3a109a9b9f1ca6df120ee90cfee
seokho10007 <sukho1007@naver.com>
2023-04-19 21:32:58 +0900

    feat: 달력에 일정을 렌더링하기 위한 view date를 반환하는 함수 추가 (#21)

---
+++ src/utils/getCalendarPlans.ts
@@ -0,0 +1,125 @@
+import moment from 'moment';
+
+import { getCurrentDateInMonth } from './dayHandler';
+import { TDateYMD } from '@/stores/date';
+import { IIndexableViewPlan, IViewPlanInfo } from '@/types';
+import { IPlan } from '@/types/rq/plan';
+
+const getViewPlans = (plan: IPlan, date: TDateYMD) => {
+  const result: IViewPlanInfo[] = [];
+
+  // 일정이 표시될 시작일을 구해야함
+  const { year, month, day } = date;
+  const { id = -1, startTime, endTime } = plan;
+
+  // start가 현재 달보다 작은 달일 경우 현재 달의 첫번째 주의 첫번째 날로 설정
+  const currentDate = moment({ year, month: month - 1, day });
+  const [currentStart, currentEnd] = getCurrentDateInMonth(currentDate);
+
+  let viewStart = moment(startTime).startOf('d');
+  let viewEnd = moment(endTime).startOf('d');
+
+  // 이번달에 첫번째 주의 시작 날짜보다 일정의 종료날짜가 더 빠르다면
+  // 이번달에 마지막 주의 종료 날짜보다 일정의 시작날짜가 더 느리다면
+  if (currentStart.isAfter(viewEnd) || currentEnd.isBefore(viewStart)) {
+    return [];
+  }
+
+  // 일정의 시작날짜가 현재달의 첫번째주 첫번째 날짜보다 작다면 현재달의 첫번째주 첫번째 날짜로 설정
+  if (viewStart.isBefore(currentStart)) {
+    viewStart = currentStart.clone();
+  }
+
+  // 일정의 종료날짜가 현재달의 마지막주 마지막 날짜보다 크다면 현재달의 마지막주 마지막 날짜로 설정
+  if (viewEnd.isAfter(currentEnd)) {
+    viewEnd = currentEnd.clone();
+  }
+
+  // 항상 절대적인 날짜 차이를 반환해야함
+  const weekDayDiff =
+    viewEnd.clone().endOf('w').diff(viewStart.clone().startOf('w'), 'days') + 1;
+  const weekDiff = Math.ceil(weekDayDiff / 7) || 1;
+
+  // 주차를 돔
+  for (let i = 0; i < weekDiff; i++) {
+    let newViewStart = null;
+    let newViewEnd = null;
+
+    // 시작날짜의 주차와 종료날짜의 주차 비교
+    if (weekDiff === 1) {
+      newViewStart = viewStart;
+      newViewEnd = viewEnd;
+    } else {
+      if (i === 0) {
+        // 첫주
+        newViewStart = viewStart;
+        newViewEnd = viewStart.clone().endOf('w');
+      } else if (i === weekDiff - 1) {
+        // 마지막주
+        newViewStart = viewEnd.clone().startOf('w');
+        newViewEnd = viewEnd;
+      } else {
+        // 중간주
+        newViewStart = viewStart.clone().add(i, 'w').startOf('w');
+        newViewEnd = viewStart.clone().add(i, 'w').endOf('w');
+      }
+    }
+
+    // 현재 달의 첫번째 주를 기준으로 몇번째 주인지
+    const weekByMonth =
+      (newViewStart.clone().endOf('w').diff(currentStart, 'days') + 1) / 7 - 1;
+
+    // 일정이 표시될 날짜의 차이
+    const dayDiff =
+      Math.abs(newViewEnd.clone().diff(newViewStart.clone(), 'days')) + 1;
+
+    result.push({
+      id,
+      dayDiff,
+      weekByMonth,
+      viewStart: newViewStart.clone(),
+      viewEnd: newViewEnd.clone(),
+      startTime: moment(startTime).startOf('d'),
+      endTime: moment(endTime).startOf('d'),
+      dayOfWeek: newViewStart.day() + 1,
+      plan: plan.id === -1 ? null : plan,
+    });
+  }
+
+  return result;
+};
+
+const getCalendarPlans = (plans: IPlan[], date: TDateYMD) => {
+  const result: IViewPlanInfo[] = plans
+    .map((el) => getViewPlans(el, date))
+    .flat()
+    .sort(
+      (a, b) =>
+        a.startTime.diff(b.startTime) || b.dayDiff - a.dayDiff || a.id - b.id,
+    );
+
+  const planViews: IIndexableViewPlan[][] = Array.from({ length: 6 }, () =>
+    Array.from({ length: 7 }, () => ({})),
+  );
+
+  result.forEach((planView) => {
+    const { weekByMonth, dayOfWeek, dayDiff } = planView;
+    let index = 0;
+
+    for (let l = 0; l < dayDiff; l++) {
+      const dayInfo = planViews[weekByMonth][dayOfWeek + l - 1];
+
+      while (dayInfo[index] !== planView) {
+        if (!dayInfo[index]) {
+          dayInfo[index] = planView;
+        } else {
+          index++;
+        }
+      }
+    }
+  });
+
+  return planViews;
+};
+
+export { getViewPlans, getCalendarPlans };
