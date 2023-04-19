52a38186bf1eb2d7701dfb6f495d7524bab4d173
jintak0401 <jintak0401@naver.com>
2023-04-19 17:39:47 +0900

    feat: 일정 생성 모달창의 태그 관련 컴포넌트 구현 (#19)

--- src/components/modal/plan/PlanTag.tsx
+++ src/components/modal/plan/PlanTag.tsx
@@ -1,19 +1,105 @@
-import React from 'react';
+import React, { FormEventHandler, useRef, useState } from 'react';
 
 import styled from '@emotion/styled';
 
+import TagButton from '@/components/buttons/TagButton';
 import Dropdown from '@/components/common/dropdown';
+import Input from '@/components/common/Input';
+import { TagIcon } from '@/components/icons';
+import ClassifierTitle from '@/components/sidebar/classifier/ClassifierTitle';
+import { MAX_CANDIDATE_LENGTH } from '@/constants';
+import { TAG_MOCK } from '@/utils/mock';
+import { Candidate } from 'components/modal/plan/Candidates';
 
-type TPlanTagProps = {};
+const PlanTag: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [tagInput, setTagInput] = useState('');
+  const [isCandidateOpened, setIsCandidateOpened] = useState(false);
+  const [selectedTags, setSelectedTags] = useState<string[]>([
+    '테스트1',
+    '테스트2',
+  ]);
 
-type TPlanTag = React.FC<TPlanTagProps>;
+  const addTag = (tag?: string) => {
+    if (!tag || selectedTags.length >= 5 || selectedTags.includes(tag)) return;
+    setSelectedTags((prev) => [...prev, tag]);
+    setTagInput('');
+  };
 
-const PlanTag = ({}: TPlanTagProps) => {
-  return <Container></Container>;
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    addTag(inputRef.current?.value);
+    inputRef.current?.blur();
+  };
+
+  return (
+    <Container>
+      <Dropdown.Controller>
+        <ClassifierTitle title="태그" titleIcon={<TagIcon />} />
+      </Dropdown.Controller>
+      {/* 입력창 */}
+      <form onSubmit={onSubmit}>
+        <Input
+          type="text"
+          ref={inputRef}
+          value={tagInput}
+          placeholder={'태그를 입력하세요'}
+          onFocus={() => setIsCandidateOpened(true)}
+          onBlur={() => setIsCandidateOpened(false)}
+          onClear={() => setTagInput('')}
+          onChange={(e) => setTagInput(e.target.value)}
+        />
+      </form>
+      {/* 추가된 태그 버튼들 */}
+      <TagButtonContainer>
+        {selectedTags.map((tag) => (
+          <TagButton
+            onClick={() =>
+              setSelectedTags((prev) => prev.filter((name) => name !== tag))
+            }
+            key={tag}
+          >
+            {tag}
+          </TagButton>
+        ))}
+      </TagButtonContainer>
+      {/* 태그 후보군 팝업창 */}
+      {isCandidateOpened && tagInput && (
+        <Candidates type={'tag'}>
+          {TAG_MOCK.filter((tag) => tag.name.match(tagInput))
+            .slice(0, MAX_CANDIDATE_LENGTH)
+            .map((candidate, index) => (
+              <Candidate.Item
+                key={`Candidate-${index}`}
+                onMouseDown={() => {
+                  addTag(candidate.name);
+                }}
+                name={candidate.name}
+                isSelected={selectedTags.includes(candidate.name)}
+              />
+            ))}
+        </Candidates>
+      )}
+    </Container>
+  );
 };
 
 const Container = styled(Dropdown)`
   width: 100%;
+  position: relative;
+`;
+
+const Candidates = styled(Candidate.List)`
+  position: absolute;
+  top: 40px;
+`;
+
+const TagButtonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  width: 100%;
+  gap: 4px;
 `;
 
 export default PlanTag;
