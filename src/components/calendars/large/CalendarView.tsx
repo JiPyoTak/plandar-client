4aa9578e2669a5af87cc6e8a74a48afad8740f5e
seokho10007 <sukho1007@naver.com>
2023-04-24 23:56:57 +0900

    feat: calendar drag and drop 기능 구현 (#21)

--- src/components/calendars/large/CalendarView.tsx
+++ src/components/calendars/large/CalendarView.tsx
@@ -1,35 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styled from '@emotion/styled';
 
 import CalendarCell from './CalendarCell';
+import CalendarLayer from './CalendarLayer';
+import { dummy } from './dummy';
+import useDrag, { MouseEventHandler } from '@/hooks/drag/useDrag';
 import useDateState from '@/stores/date';
+import useSelectedPlanState from '@/stores/plan/selectedPlan';
+import { IViewPlanInfo } from '@/types';
+import { getStartAndEndDateInMonth } from '@/utils/dayHandler';
 import { getCalendarInfo } from '@/utils/getCalendarInfo';
+import { getCalendarPlans } from '@/utils/getCalendarPlans';
 
 const CalendarBody = () => {
+  const { selectedPlan, selectPlan } = useSelectedPlanState();
   const { onChangeStoreDate, year, month, day } = useDateState();
-  const calendarInfos = getCalendarInfo({ year, month, day });
+  const [isDragging, changeCurrentDate, onMouseMove] = useDrag();
 
-  const onClickDayContent = () => {
-    console.log('onClickDayContent');
+  const calendarInfos = getCalendarInfo({ year, month, day });
+  const [startDate, endDate] = getStartAndEndDateInMonth(calendarInfos);
+  const calendarPlanViews = getCalendarPlans(dummy, startDate, endDate);
+
+  const [selectedPlanViews, setSelectedPlanViews] = useState<
+    IViewPlanInfo[][][] | null
+  >(null);
+
+  const onMouseDownCell: MouseEventHandler = (e) => {
+    e.stopPropagation();
+
+    changeCurrentDate(e);
+
+    const target = document
+      .elementsFromPoint(e.clientX, e.clientY)
+      .find((el) => el.classList.contains('dateTime')) as HTMLElement;
+
+    const targetDate = target?.dataset?.date;
+
+    if (!targetDate) return;
+
+    const newPlan = {
+      id: -1,
+      startTime: targetDate,
+      endTime: targetDate,
+    };
+
+    selectPlan(newPlan);
   };
 
+  useEffect(() => {
+    if (!selectedPlan) return setSelectedPlanViews(null);
+
+    const newPlan = getCalendarPlans([selectedPlan], startDate, endDate);
+
+    setSelectedPlanViews(newPlan);
+  }, [selectedPlan]);
+
   return (
-    <Container>
+    <Container
+      className={isDragging ? 'isDragging' : ''}
+      onMouseMove={isDragging ? onMouseMove : undefined}
+      onMouseDown={changeCurrentDate}
+    >
       {calendarInfos.map((week, i) => (
-        <CalendarBody.Week key={`${week[0].day}${i}`}>
-          {week.map((dateInfo) => (
-            <CalendarBody.Cell
-              key={`${dateInfo.month}${dateInfo.day}`}
-              isLastWeek={i === calendarInfos.length - 1}
-              isLastDay={dateInfo.day === week[week.length - 1].day}
-              dateInfo={dateInfo}
-              isSelected={dateInfo.day === day && dateInfo.isInMonth}
-              onClickDayNumber={onChangeStoreDate}
-              onClickDayContent={onClickDayContent}
-            />
-          ))}
-        </CalendarBody.Week>
+        <CalendarBody.Inner key={`${week[i].day}${i}`}>
+          <CalendarBody.Inner>
+            {week.map((dateInfo, j) => (
+              <CalendarBody.Cell
+                key={`${dateInfo.month}${dateInfo.day}`}
+                height={calendarPlanViews[i][j].length * 24}
+                isLastWeek={i === calendarInfos.length - 1}
+                isLastDay={dateInfo.day === week[week.length - 1].day}
+                dateInfo={dateInfo}
+                format={dateInfo.format}
+                isSelected={dateInfo.day === day && dateInfo.isInMonth}
+                onClickDayNumber={onChangeStoreDate}
+                onMouseDown={onMouseDownCell}
+              />
+            ))}
+          </CalendarBody.Inner>
+          <CalendarLayer
+            calendarPlanView={calendarPlanViews?.[i]}
+            selectedPlanView={selectedPlanViews?.[i]}
+          />
+        </CalendarBody.Inner>
       ))}
     </Container>
   );
@@ -43,14 +97,21 @@ const Container = styled.div`
   display: flex;
   flex-flow: column;
   align-items: stretch;
+
+  &.isDragging,
+  &.isDragging * {
+    cursor: grabbing !important;
+  }
 `;
 
-const Week = styled.div`
+const Inner = styled.div`
+  position: relative;
+
   flex: 1;
   display: flex;
 `;
 
 CalendarBody.Cell = CalendarCell;
-CalendarBody.Week = Week;
+CalendarBody.Inner = Inner;
 
 export default CalendarBody;
