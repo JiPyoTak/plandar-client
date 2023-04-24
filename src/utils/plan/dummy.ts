5dfeea69028e0cb6e613ae837de5f6b5e1c6eed0
seokho10007 <sukho1007@naver.com>
2023-04-25 01:55:32 +0900

    feat: plan dummy data 추가

---
+++ src/utils/plan/dummy.ts
@@ -0,0 +1,28 @@
+import { IPlan } from '@/types/rq/plan';
+
+export const dummy = [
+  {
+    id: 0,
+    title: 'test',
+    startTime: '2023-04-07T07:04:19.883Z',
+    endTime: '2023-04-20T07:04:19.883Z',
+  },
+  {
+    id: 1,
+    title: 'test',
+    startTime: '2023-04-13T07:04:19.883Z',
+    endTime: '2023-04-14T07:04:19.883Z',
+  },
+  {
+    id: 2,
+    title: 'test',
+    startTime: '2023-04-03T07:04:19.883Z',
+    endTime: '2023-04-03T07:04:19.883Z',
+  },
+  {
+    id: 4,
+    title: 'test',
+    startTime: '2023-03-31T07:04:19.883Z',
+    endTime: '2023-04-01T07:04:19.883Z',
+  },
+] as IPlan[];
