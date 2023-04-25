91cf6792e11f74f73c34683140581bce4043f549
jintak0401 <jintak0401@naver.com>
2023-04-25 16:20:12 +0900

    feat: 일정생성 모달 내에서 새 카테고리를 생성하는 컴포넌트 구현 (#19)

---
+++ src/components/modal/plan/CategoryCreate.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+import styled from '@emotion/styled';
+
+import { SELECTABLE_COLOR } from '@/constants';
+import { useCategoryCreate } from '@/hooks/rq/category';
+import { FONT_REGULAR_3 } from '@/styles/font';
+import { ICategoryWithoutId } from '@/types/rq/category';
+
+type TCategoryCreateProps = {
+  name: string;
+  onFocus: () => void;
+  onSuccess: (newCategory: ICategoryWithoutId) => void;
+};
+
+type TCategoryCreate = React.FC<TCategoryCreateProps>;
+
+const CategoryCreate: TCategoryCreate = ({
+  name,
+  onFocus,
+  onSuccess,
+}: TCategoryCreateProps) => {
+  const [error, setError] = useState('');
+  const { mutateAsync: createCategory } = useCategoryCreate();
+  const newCategoryName = name.trim();
+
+  const onClick = async () => {
+    try {
+      const newCategory = await createCategory({
+        name: newCategoryName,
+        color: SELECTABLE_COLOR[0],
+      });
+      onSuccess(newCategory);
+    } catch (e) {
+      setError('카테고리 생성에 실패했습니다.');
+      // todo: 토스트 메시지 띄워주기
+    }
+  };
+
+  return (
+    <Container onFocus={onFocus}>
+      <span>"{newCategoryName}"</span>
+      <Button onMouseDown={onClick}>카테고리 생성하기</Button>
+      <Warning>{error}</Warning>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 20px 0;
+  width: 100%;
+  text-align: center;
+  color: ${({ theme }) => theme.placeholder};
+  ${FONT_REGULAR_3}
+`;
+
+const Button = styled.button`
+  color: inherit;
+  font: inherit;
+  width: fit-content;
+
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const Warning = styled.span`
+  color: ${({ theme }) => theme.red};
+`;
+
+export default CategoryCreate;
