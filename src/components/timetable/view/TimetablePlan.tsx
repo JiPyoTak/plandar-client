54f6764169936ec6716a15dc4fb835fbed981301
pyo-sh <pygosky@gmail.com>
2023-04-25 17:10:55 +0900

    feat: Tiemtable Cell과 Plan Hover에 대해서 UX 개선 (transition, z-index) (#20)

--- src/components/timetable/view/TimetablePlan.tsx
+++ src/components/timetable/view/TimetablePlan.tsx
@@ -7,7 +7,7 @@ import moment from 'moment';
 
 import { TIMETABLE_CELL_UNIT } from '@/constants';
 import { FONT_REGULAR_7, FONT_REGULAR_8 } from '@/styles/font';
-import { TIMETABLE_CELL_HEIGHT } from '@/styles/timetable';
+import { TIMETABLE_CELL_HEIGHT, TIMETABLE_Z_INDEX } from '@/styles/timetable';
 import { TColor } from '@/types';
 import { ITimePlan } from '@/types/rq/plan';
 import { isBgBright } from '@/utils/color';
@@ -88,14 +88,15 @@ const Container = styled.div`
   padding: 0.125rem 0.25rem;
 
   position: absolute;
-  z-index: 2;
+  z-index: ${TIMETABLE_Z_INDEX['timePlan']};
 
   border: 1px solid white;
   border-radius: 8px;
   overflow: hidden;
+  cursor: pointer;
 
   &:hover {
-    z-index: 3;
+    z-index: ${TIMETABLE_Z_INDEX['timePlanHover']};
   }
 `;
 
