e6b694c96aa29071e1282469cba995a09793a3c6
pyo-sh <pygosky@gmail.com>
2023-04-27 17:44:08 +0900

    fix: Storybook에서 해당 달력을 렌더링 할 때 전역 상태를 변경하기 (#47)

--- src/stories/calendars/LargeCalendar.stories.tsx
+++ src/stories/calendars/LargeCalendar.stories.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import CalendarView from '@/components/calendars/large';
 import CalendarHeader from '@/components/common/calendar/CalendarHeader';
+import { CALENDAR_UNIT } from '@/constants';
+import useCalendarUnitState from '@/stores/date/calendarUnit';
 
 export default {
   title: 'calendars/LargeCalendar',
   component: CalendarView,
 } as ComponentMeta<typeof CalendarView>;
 
-const Template: ComponentStory<typeof CalendarView> = () => <CalendarView />;
+const Template: ComponentStory<typeof CalendarView> = () => {
+  const { selectCalendarUnit } = useCalendarUnitState();
+  useEffect(() => {
+    selectCalendarUnit(CALENDAR_UNIT[2]);
+  }, []);
+
+  return <CalendarView />;
+};
 const Template2 = () => <CalendarHeader />;
 
 export const View = Template.bind({});
