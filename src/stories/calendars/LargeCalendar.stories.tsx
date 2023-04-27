93709fce7a396bbc493b364b220997021509dc23
pyo-sh <pygosky@gmail.com>
2023-04-27 17:41:13 +0900

    fix: LargeCalendar storybook에서 Main 템플릿 삭제 (#47)

--- src/stories/calendars/LargeCalendar.stories.tsx
+++ src/stories/calendars/LargeCalendar.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import styled from '@emotion/styled';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import CalendarView from '@/components/calendars/large';
@@ -13,37 +12,9 @@ export default {
 
 const Template: ComponentStory<typeof CalendarView> = () => <CalendarView />;
 const Template2 = () => <CalendarHeader />;
-const Template3 = () => {
-  return (
-    <Container>
-      <CalendarHeader />
-      <section>
-        <CalendarView />
-      </section>
-    </Container>
-  );
-};
-
-const Container = styled.div`
-  display: flex;
-
-  height: 100%;
-
-  flex-direction: column;
-
-  & > section {
-    background-color: ${({ theme }) => theme.primary_light3};
-    flex: 1;
-    display: flex;
-    padding: 1rem;
-  }
-`;
 
 export const View = Template.bind({});
 View.args = {};
 
 export const Header = Template2.bind({});
 Template2.args = {};
-
-export const Main = Template3.bind({});
-Template3.args = {};
