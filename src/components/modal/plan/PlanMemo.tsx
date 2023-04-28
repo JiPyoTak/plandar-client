7799a7824a3bf741dfbae9c0dac47aeb10e7717d
jintak0401 <jintak0401@naver.com>
2023-04-28 15:33:25 +0900

    feat: TitleIcon 지정 및 memo를 상태로 관리하도록 변경 (#19)

--- src/components/modal/plan/PlanMemo.tsx
+++ src/components/modal/plan/PlanMemo.tsx
@@ -1,14 +1,9 @@
-import React, {
-  ChangeEventHandler,
-  TextareaHTMLAttributes,
-  useRef,
-} from 'react';
+import React, { ChangeEventHandler, useRef, useState } from 'react';
 
 import styled from '@emotion/styled';
 
 import Dropdown from '@/components/common/dropdown';
 import { MemoIcon } from '@/components/icons';
-import ClassifierTitle from '@/components/sidebar/classifier/ClassifierTitle';
 import { MAX_MEMO_LENGTH } from '@/constants';
 import { FONT_REGULAR_4 } from '@/styles/font';
 import {
@@ -17,12 +12,9 @@ import {
   PlanModalClassifierTitle,
 } from '@/styles/planModal';
 
-type TPlanMemoProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
-
-type TPlanMemo = React.FC<TPlanMemoProps>;
-
-const PlanMemo: TPlanMemo = ({ onChange, ...rest }: TPlanMemoProps) => {
+const PlanMemo: React.FC = () => {
   const ref = useRef<HTMLTextAreaElement>(null);
+  const [memo, setMemo] = useState('');
 
   const handleTextareaHeight = () => {
     const current = ref.current;
@@ -34,7 +26,7 @@ const PlanMemo: TPlanMemo = ({ onChange, ...rest }: TPlanMemoProps) => {
   const onChangeTextArea: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     handleTextareaHeight();
     if (e.target.value.length <= MAX_MEMO_LENGTH) {
-      onChange?.(e);
+      setMemo(e.target.value);
     }
   };
 
@@ -43,10 +35,10 @@ const PlanMemo: TPlanMemo = ({ onChange, ...rest }: TPlanMemoProps) => {
       <Dropdown.Controller>
         <PlanModalClassifierTitle
           title="메모"
-          titleIcon={<MemoIcon />}
+          titleIcon={<MemoIcon width="18" height="18" />}
           additionalComponent={
             <ContentLenDisplay>
-              {ref.current?.value.length ?? 0} / {MAX_MEMO_LENGTH}
+              {memo.length} / {MAX_MEMO_LENGTH}
             </ContentLenDisplay>
           }
         />
@@ -55,8 +47,8 @@ const PlanMemo: TPlanMemo = ({ onChange, ...rest }: TPlanMemoProps) => {
         <TextArea
           placeholder={`메모를 최대 ${MAX_MEMO_LENGTH}자까지 입력할 수 있습니다`}
           ref={ref}
+          value={memo}
           onChange={onChangeTextArea}
-          {...rest}
         />
       </div>
     </Container>
@@ -76,6 +68,7 @@ const TextArea = styled.textarea`
   font-family: inherit;
   line-height: 1.5;
   padding: 0 24px 0;
+  height: 18px;
   ${FONT_REGULAR_4}
 
   &::placeholder {
