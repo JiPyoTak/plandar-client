3bcaf16d55d13602dcf6c4e4e6ec1feb3cededc7
jintak0401 <jintak0401@naver.com>
2023-05-02 15:47:12 +0900

    feat: getCategoryAPI 함수 return 타입 변경 (#52)

--- src/apis/category.ts
+++ src/apis/category.ts
@@ -1,23 +1,21 @@
 import { axiosAPI } from '@/apis/axios';
 import { ICategory, ICategoryWithoutId } from '@/types/rq/category';
 
-const getCategoryAPI = async (): Promise<{
-  data: ICategory[];
-}> => {
+const getCategoryAPI = async (): Promise<ICategory[]> => {
   const { data } = await axiosAPI.get(`/category`);
-  return data;
+  return data.data;
 };
 
 const createCategoryAPI = async (
   category: ICategoryWithoutId,
 ): Promise<ICategory> => {
   const { data } = await axiosAPI.post(`/category`, category);
-  return data;
+  return data.data;
 };
 
 const updateCategoryAPI = async (category: ICategory): Promise<ICategory> => {
   const { data } = await axiosAPI.put(`/category/${category.id}`, category);
-  return data;
+  return data.data;
 };
 
 export { getCategoryAPI, createCategoryAPI, updateCategoryAPI };
