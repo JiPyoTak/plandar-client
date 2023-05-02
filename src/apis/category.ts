2ee261b94bdba5b454e91260736182609919e1ff
jintak0401 <jintak0401@naver.com>
2023-05-02 16:20:58 +0900

    feat: axios response interceptor 적용된 형태로 변경 (#52)

--- src/apis/category.ts
+++ src/apis/category.ts
@@ -3,19 +3,19 @@ import { ICategory, ICategoryWithoutId } from '@/types/rq/category';
 
 const getCategoryAPI = async (): Promise<ICategory[]> => {
   const { data } = await axiosAPI.get(`/category`);
-  return data.data;
+  return data;
 };
 
 const createCategoryAPI = async (
   category: ICategoryWithoutId,
 ): Promise<ICategory> => {
   const { data } = await axiosAPI.post(`/category`, category);
-  return data.data;
+  return data;
 };
 
 const updateCategoryAPI = async (category: ICategory): Promise<ICategory> => {
   const { data } = await axiosAPI.put(`/category/${category.id}`, category);
-  return data.data;
+  return data;
 };
 
 export { getCategoryAPI, createCategoryAPI, updateCategoryAPI };
