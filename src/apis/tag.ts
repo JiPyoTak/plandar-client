a82325410712f1b9bfcec118701f30ecbfb72abe
jintak0401 <jintak0401@naver.com>
2023-05-03 14:01:31 +0900

    feat: tag api 함수 구현 (#52)

--- src/apis/tag.ts
+++ src/apis/tag.ts
@@ -3,17 +3,17 @@ import { ITag, ITagWithoutId } from '@/types/rq/tag';
 
 const getTagAPI = async (): Promise<ITag[]> => {
   const { data } = await axiosAPI.get(`/tag`);
-  return data.data;
+  return data;
 };
 
 const createTagAPI = async (category: ITagWithoutId): Promise<ITag> => {
   const { data } = await axiosAPI.post(`/tag`, category);
-  return data.data;
+  return data;
 };
 
 const updateTagAPI = async (tag: ITag): Promise<ITag> => {
   const { data } = await axiosAPI.put(`/tag/${tag.id}`, tag);
-  return data.data;
+  return data;
 };
 
 export { getTagAPI, createTagAPI, updateTagAPI };
