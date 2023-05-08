d8a6b0a752149c87333ce547a2da099786aaa897
pyo-sh <pygosky@gmail.com>
2023-05-08 14:03:12 +0900

    refactor: TimePlanManager를 통해 TimeTable 값을 렌더링 할 수 있도록 수정 (#59)

--- src/components/timetable/view/TimetablePlanColumn.tsx
+++ src/components/timetable/view/TimetablePlanColumn.tsx
@@ -2,26 +2,26 @@ import React from 'react';
 
 import TimePlan from '@/components/plan/TimePlan';
 import Plan from '@/plan/Plan';
-import { getOrderTimePlansInfo } from '@/utils/plan/day/getOrderTimePlansInfo';
+import TimePlanManager from '@/plan/TimePlanManager';
 
 type TProps = {
   plans: Plan[];
 };
 
 const TimetablePlanColumn: React.FC<TProps> = ({ plans }) => {
-  const orderInfos = getOrderTimePlansInfo(plans);
+  const planManager = new TimePlanManager(plans);
 
   return (
     <div css={{ width: 'calc(100% - 0.75rem)', position: 'relative' }}>
       <div css={{ width: '100%', position: 'absolute' }}>
-        {plans.map((plan, index) => (
-          <TimePlan
-            key={plan.id ?? `timetable-plan-${index}`}
-            plan={plan}
-            rank={orderInfos[index].rank}
-            total={orderInfos[index].total}
-          />
-        ))}
+        {plans.map((plan) => {
+          const { id } = plan;
+          const viewInfo = planManager.viewInfo.get(id);
+
+          return (
+            viewInfo && <TimePlan key={id} plan={plan} viewInfo={viewInfo} />
+          );
+        })}
       </div>
     </div>
   );
