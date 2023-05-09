27763338ef449d7b3ff5bea743c64593f2a183eb
seokho10007 <sukho1007@naver.com>
2023-05-09 17:05:25 +0900

    feat: 달력의 주차 범위에 따라 PlanManager을 반환하는 함수 구현 (#61)

---
+++ src/utils/plan/getDaysPlanManager.ts
@@ -0,0 +1,43 @@
+import DaysPlanManager from '@/plan/DaysPlanManager';
+import Plan from '@/plan/Plan';
+import { TDateYMD } from '@/stores/date';
+import { IPlan } from '@/types/rq/plan';
+import { ICalendarInfo } from '@/utils/calendar/getCalendarInfo';
+
+const getYMD = <T extends TDateYMD>(date: T) => {
+  return {
+    year: date.year,
+    month: date.month - 1,
+    day: date.day,
+  };
+};
+
+const getDaysPlanManager = (
+  plans: IPlan[],
+  calendarInfos: ICalendarInfo[][],
+) => {
+  const arr: DaysPlanManager[] = [];
+
+  const planClass = plans
+    .filter((plan) => plan.type === 'task')
+    .map((plan) => new Plan(plan));
+
+  for (let i = 0; i < calendarInfos.length; i++) {
+    const week = calendarInfos[i];
+
+    const firstDay = getYMD(week[0]);
+    const lastDay = getYMD(week[week.length - 1]);
+
+    const viewPlans = new DaysPlanManager({
+      end: lastDay,
+      start: firstDay,
+      plans: planClass,
+    });
+
+    arr.push(viewPlans);
+  }
+
+  return arr;
+};
+
+export { getDaysPlanManager };
