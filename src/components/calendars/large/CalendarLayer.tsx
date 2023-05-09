aeb7c12caf06b32d6bda9505979b327c7f50fc35
seokho10007 <sukho1007@naver.com>
2023-05-09 17:00:48 +0900

    feat: planManager에 저장된 plan을 보여주는 CalendarLayer 생성 (#61)

---
+++ src/components/calendars/large/CalendarLayer.tsx
@@ -0,0 +1,85 @@
+import React, { memo, useCallback } from 'react';
+
+import styled from '@emotion/styled';
+
+import DayPlan from '@/components/plan/DayPlan';
+import DaysPlanManager from '@/plan/DaysPlanManager';
+import Plan from '@/plan/Plan';
+import useFocusedPlanState from '@/stores/plan/focusedPlan';
+import useHoveredPlanState from '@/stores/plan/hoveredPlan';
+
+interface IProps {
+  planManager: DaysPlanManager;
+}
+
+const CalendarLayer = ({ planManager }: IProps) => {
+  const { focusedPlan, selectPlan } = useFocusedPlanState();
+  const { hoveredPlan, setHoveredPlan, clearHoveredPlan } =
+    useHoveredPlanState();
+
+  const plans = planManager.plans;
+  const viewPlans = planManager.viewInfo;
+
+  const onMouseEnter = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>, plan: Plan) => {
+      if (focusedPlan) return;
+
+      const target = e.currentTarget as HTMLElement;
+
+      const { top, left, right, bottom } = target.getBoundingClientRect();
+
+      setHoveredPlan({
+        hoveredPlan: plan,
+        rect: { top, left, right, bottom },
+      });
+    },
+    [focusedPlan?.id],
+  );
+
+  const onMouseDown = useCallback((plan: Plan) => {
+    selectPlan(plan);
+    clearHoveredPlan();
+  }, []);
+
+  const onMouseLeave = useCallback(() => {
+    clearHoveredPlan();
+  }, []);
+
+  return (
+    <Container>
+      {plans.map((plan, i) => {
+        const viewPlan = viewPlans.get(plan.id);
+
+        if (!viewPlan) return null;
+
+        return (
+          <DayPlan
+            key={i}
+            plan={plan}
+            view={viewPlan}
+            isSelected={focusedPlan?.id === plan.id}
+            isHovered={hoveredPlan?.id === plan.id}
+            onMouseEnter={onMouseEnter}
+            onMouseDown={onMouseDown}
+            onMouseLeave={onMouseLeave}
+          />
+        );
+      })}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: absolute;
+  top: 36px;
+  left: 0;
+  right: 0;
+  z-index: 10;
+
+  & > div {
+    position: absolute;
+    padding: 0 8px;
+  }
+`;
+
+export default memo(CalendarLayer);
