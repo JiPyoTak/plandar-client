b0fbf8ed9eb37a5c4a606b4e7fc2139edbd31da0
seokho10007 <sukho1007@naver.com>
2023-05-09 17:03:20 +0900

    feat: 달력의 주차를 표시하는 CalendarWeek 컴포넌트 추가

---
+++ src/components/calendars/large/CalendarWeek.tsx
@@ -0,0 +1,45 @@
+import React, { MouseEventHandler, memo } from 'react';
+
+import styled from '@emotion/styled';
+
+import CalendarCell from './CalendarCell';
+import useDateState from '@/stores/date';
+import { ICalendarInfo } from '@/utils/calendar/getCalendarInfo';
+
+interface IProps {
+  index: number;
+  week: ICalendarInfo[];
+  daysIndex: number[][];
+  onMouseDown: MouseEventHandler;
+}
+
+const CalendarWeek = ({ week, index, daysIndex, onMouseDown }: IProps) => {
+  const { day, onChangeStoreDate } = useDateState();
+
+  return (
+    <Container>
+      {week.map((dateInfo, i) => (
+        <CalendarCell
+          key={`${dateInfo.month}${dateInfo.day}`}
+          height={daysIndex[i].length * 24}
+          isLastWeek={index === 5}
+          isLastDay={i === 6}
+          dateInfo={dateInfo}
+          format={dateInfo.format}
+          isSelected={dateInfo.day === day && dateInfo.isInMonth}
+          onClickDayNumber={onChangeStoreDate}
+          onMouseDown={onMouseDown}
+        />
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+
+  flex: 1;
+  display: flex;
+`;
+
+export default memo(CalendarWeek);
