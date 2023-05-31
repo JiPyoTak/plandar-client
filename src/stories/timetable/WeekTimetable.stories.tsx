d2fadcfac159be82f082412ccca3edee4c373df0
pyo-sh <pygosky@gmail.com>
2023-05-31 16:31:15 +0900

    test: Storybook에서 Timetable 일정 테스팅을 확실히 할 수 있도록 변경 (#76)

--- src/stories/timetable/WeekTimetable.stories.tsx
+++ src/stories/timetable/WeekTimetable.stories.tsx
@@ -30,7 +30,7 @@ const Template: ComponentStory<typeof Timetable> = (args) => {
     selectCalendarUnit('주');
   }, []);
 
-  const addRangePlan = () => {
+  const addRandomAlldayPlan = () => {
     const startOfWeek = moment(`${year}-${month}-${day}`)
       .startOf('week')
       .get('day');
@@ -52,6 +52,34 @@ const Template: ComponentStory<typeof Timetable> = (args) => {
     setId((prevId) => prevId + 1);
   };
 
+  const addRandomTimePlan = () => {
+    const startOfWeek = moment(`${year}-${month}-${day}`).startOf('week');
+    const dayPeriod = Math.round(Math.random() * 6);
+    const startHour = Math.round(Math.random() * 21);
+    const startMinute = Math.round(Math.random() * 59);
+    const startPeriod = startHour * 60 + startMinute;
+
+    const startTime = moment(startOfWeek)
+      .add(dayPeriod, 'days')
+      .add(startPeriod, 'minutes');
+
+    const periodMinutes = Math.round(Math.random() * (24 * 60 - 16)) + 15;
+    const endTime = moment(startTime).add(periodMinutes, 'minutes');
+
+    MONTH_PLANS_MOCK[month] = [
+      ...MONTH_PLANS_MOCK[month],
+      createPlanMock({
+        id,
+        title: `임시 데이터 ${id}`,
+        isAllDay: false,
+        startTime,
+        endTime,
+      }),
+    ];
+
+    setId((prevId) => prevId + 1);
+  };
+
   const clearPlans = () => {
     MONTH_PLANS_MOCK[month] = [];
     setId(1);
@@ -60,8 +88,11 @@ const Template: ComponentStory<typeof Timetable> = (args) => {
   return (
     <Container>
       <div className="day-timetable-controls">
-        <TestButton onClick={addRangePlan}>
+        <TestButton onClick={addRandomAlldayPlan}>
           범위 안 종일 일정 추가하기
+        </TestButton>
+        <TestButton onClick={addRandomTimePlan}>
+          범위 안 시간 일정 추가하기
         </TestButton>
         <TestButton onClick={clearPlans}>해당 달의 일정 삭제하기</TestButton>
       </div>
