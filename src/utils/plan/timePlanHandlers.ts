2732bffbe7df6f498aae439de354b85cadecca66
pyo-sh <pygosky@gmail.com>
2023-06-01 16:05:48 +0900

    feat: 시간 일정의 크기를 지정할 때 24시간을 넘기지 못하게 설정 (#81)

--- src/utils/plan/timePlanHandlers.ts
+++ src/utils/plan/timePlanHandlers.ts
@@ -12,16 +12,41 @@ type TProps = {
   type: IChangePlanViewType;
 };
 
+const DAY_MAX_MINUTE = 24 * 60;
+
 const getCreatedTimePlan = (props: TProps) => {
   const { targetDate, currentDate, focusedPlan } = props;
 
   let startTime = moment(currentDate);
   let endTime = moment(targetDate);
 
-  if (startTime.isAfter(endTime)) {
+  // startTime이 endTime보다 이후라면 (시간을 늘리는 것이라면)
+  // startTime이 targetDate이여야 한다
+  const isTimeAdd = startTime.isAfter(endTime);
+  if (isTimeAdd) {
     [startTime, endTime] = [endTime, startTime];
   }
+
+  // startTime과 endTime 사이의 값이 최소 15분이 보장되어야 함
   endTime.add(TIMETABLE_CELL_UNIT, 'minutes');
+
+  // startTime과 endTime 사이의 값이 24시간을 넘으면 안됨
+  const minuteDiff = Math.abs(startTime.diff(endTime, 'minutes'));
+  if (minuteDiff > DAY_MAX_MINUTE) {
+    // 바뀐 값도 24시간이 넘고 이전에도 24시간을 넘는다면 이전 값 Return
+    const previousMinuteDiff = Math.abs(
+      focusedPlan.startMoment.diff(focusedPlan.endMoment, 'minutes'),
+    );
+    if (previousMinuteDiff === DAY_MAX_MINUTE) {
+      return focusedPlan;
+    }
+
+    if (isTimeAdd) {
+      startTime = moment(endTime).add(-DAY_MAX_MINUTE, 'minutes');
+    } else {
+      endTime = moment(startTime).add(DAY_MAX_MINUTE, 'minutes');
+    }
+  }
 
   const plan = new Plan(focusedPlan);
   plan._startTime = startTime;
