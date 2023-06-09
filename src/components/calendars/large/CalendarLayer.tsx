86eaad3f518e77e59016e249e42cbd98aab2430d
seokho10007 <sukho1007@naver.com>
2023-06-09 14:48:42 +0900

    feat: zustand shallow를 통한 state 최적화 (#94)

--- src/components/calendars/large/CalendarLayer.tsx
+++ src/components/calendars/large/CalendarLayer.tsx
@@ -2,6 +2,8 @@ import React, { memo, useCallback } from 'react';
 
 import styled from '@emotion/styled';
 
+import { shallow } from 'zustand/shallow';
+
 import DayPlan from '@/components/plan/DayPlan';
 import DaysPlanManager from '@/plan/DaysPlanManager';
 import Plan from '@/plan/Plan';
@@ -13,16 +15,31 @@ interface IProps {
 }
 
 const CalendarLayer = ({ planManager }: IProps) => {
-  const { focusedPlan, moveDragPlan } = useFocusedPlanState();
-  const { hoveredPlan, setHoveredPlan, clearHoveredPlan } =
-    useHoveredPlanState();
+  const { isDragging, focusedPlanId, moveDragPlan } = useFocusedPlanState(
+    (store) => ({
+      isDragging: store.isDragging,
+      focusedPlanId: store.focusedPlan?.id,
+      moveDragPlan: store.moveDragPlan,
+    }),
+    shallow,
+  );
+
+  const { hoveredPlanId, setHoveredPlan, clearHoveredPlan } =
+    useHoveredPlanState(
+      (store) => ({
+        hoveredPlanId: store.hoveredPlan?.id,
+        setHoveredPlan: store.setHoveredPlan,
+        clearHoveredPlan: store.clearHoveredPlan,
+      }),
+      shallow,
+    );
 
   const plans = planManager.plans;
   const viewPlans = planManager.viewInfo;
 
   const onMouseEnter = useCallback(
     (e: React.MouseEvent<HTMLDivElement>, plan: Plan) => {
-      if (focusedPlan) return;
+      if (isDragging) return;
 
       const target = e.currentTarget as HTMLElement;
 
@@ -33,7 +50,7 @@ const CalendarLayer = ({ planManager }: IProps) => {
         rect: { top, left, right, bottom },
       });
     },
-    [focusedPlan?.id],
+    [isDragging],
   );
 
   const onMouseDown = useCallback((plan: Plan) => {
@@ -53,8 +70,8 @@ const CalendarLayer = ({ planManager }: IProps) => {
             key={i}
             plan={plan}
             view={viewPlan}
-            isSelected={focusedPlan?.id === plan.id}
-            isHovered={hoveredPlan?.id === plan.id}
+            isSelected={focusedPlanId === plan.id}
+            isHovered={hoveredPlanId === plan.id}
             onMouseEnter={onMouseEnter}
             onMouseDown={onMouseDown}
             onMouseLeave={clearHoveredPlan}
