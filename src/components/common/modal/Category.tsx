2b0c3e7bd3c6a0b835daa4c7ab5b41b9b0015630
seokho10007 <sukho1007@naver.com>
2023-06-14 14:48:34 +0900

    feat: selected modal Category component 추가 (#43)

---
+++ src/components/common/modal/Category.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+import { COLOR } from './styles';
+import { useCategoryQuery } from '@/hooks/rq/category';
+
+interface IProps {
+  categoryId: number;
+}
+
+const Category: React.FC<IProps> = ({ categoryId }) => {
+  const { data: categoryData } = useCategoryQuery();
+
+  const category = categoryData?.find((category) => category.id === categoryId);
+
+  if (!category) return null;
+
+  const { color, name } = category;
+
+  return (
+    <Container>
+      <div
+        css={COLOR({
+          width: 12,
+          height: 12,
+          backgroundColor: color,
+        })}
+      />
+      <span>{name}</span>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: fit-content;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 5px;
+
+  border-radius: 5px;
+  padding: 5px 9px;
+  background-color: ${({ theme }) => theme.background2};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.background3};
+  }
+`;
+
+export default Category;
