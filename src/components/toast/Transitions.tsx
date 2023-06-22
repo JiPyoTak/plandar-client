8ef66c52585b90ec53f66f2a9643696e9f3f40a6
jintak0401 <jintak0401@naver.com>
2023-06-22 14:39:56 +0900

    feat: 토스트 메시지 애니메이션을 위한 컴포넌트 구현 (#103)

---
+++ src/components/toast/Transitions.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
+
+import { TOAST_COLLAPSE_DURATION } from '@/styles/toast';
+import { IToastTransitionProps } from '@/types/toast';
+import { collapseToast } from '@/utils/toast/collapseToast';
+
+type TAnimationStep = 'enter' | 'exit';
+
+export interface IProps extends IToastTransitionProps {
+  collapseDuration?: number;
+}
+
+// 애니메이션을 위한 className을 자식 노드에 추가
+const ToastTransition = ({
+  collapseDuration = TOAST_COLLAPSE_DURATION,
+  children,
+  done,
+  nodeRef,
+  isIn,
+}: IProps) => {
+  const enterClassName = 'bounce-enter';
+  const exitClassName = 'bounce-exit';
+  const animationStep = useRef<TAnimationStep>('enter');
+
+  useLayoutEffect(() => {
+    const node = nodeRef.current!;
+    const classToToken = enterClassName.split(' ');
+
+    const onEntered = (e: AnimationEvent) => {
+      if (e.target !== nodeRef.current) return;
+
+      node.removeEventListener('animationend', onEntered);
+      node.removeEventListener('animationcancel', onEntered);
+      if (animationStep.current === 'enter' && e.type !== 'animationcancel') {
+        node.classList.remove(...classToToken);
+      }
+    };
+
+    const onEnter = () => {
+      node.classList.add(...classToToken);
+      node.addEventListener('animationend', onEntered);
+      node.addEventListener('animationcancel', onEntered);
+    };
+
+    onEnter();
+  }, []);
+
+  useEffect(() => {
+    const node = nodeRef.current!;
+
+    const onExited = () => {
+      node.removeEventListener('animationend', onExited);
+      collapseToast(node, done, collapseDuration);
+    };
+
+    const onExit = () => {
+      animationStep.current = 'exit';
+      node.className += ` ${exitClassName}`;
+      node.addEventListener('animationend', onExited);
+    };
+
+    if (!isIn) onExit();
+  }, [isIn]);
+
+  return <>{children}</>;
+};
+
+export default ToastTransition;
