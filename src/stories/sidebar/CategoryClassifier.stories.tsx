e9025921f642cd83649373217b06717a4ab08c17
pyo-sh <pygosky@gmail.com>
2023-06-23 15:28:01 +0900

    feat: CategoryClassifier -> 새로운 카테고리 추가 테스트 (#117)

--- src/stories/sidebar/CategoryClassifier.stories.tsx
+++ src/stories/sidebar/CategoryClassifier.stories.tsx
@@ -1,11 +1,14 @@
+import styled from '@emotion/styled';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import CategoryClassifier from '@/components/sidebar/CategoryClassifier';
+import { useCategoryCreate } from '@/hooks/rq/category';
 import {
   createCategoryAPIHandler,
   getCategoryAPIHandler,
   updateCategoryAPIHandler,
 } from '@/stories/apis/category';
+import categoryStubManager from '@/stories/apis/data/category';
 
 export default {
   title: 'Sidebar/Classifier',
@@ -13,8 +16,34 @@ export default {
 } as ComponentMeta<typeof CategoryClassifier>;
 
 const Template: ComponentStory<typeof CategoryClassifier> = (args) => {
-  return <CategoryClassifier {...args} />;
+  const { mutateAsync } = useCategoryCreate();
+
+  const createCategory = () => {
+    mutateAsync(categoryStubManager.createStub());
+  };
+
+  return (
+    <div>
+      <Controls>
+        <TestButton onClick={createCategory}>카테고리 추가하기</TestButton>
+      </Controls>
+      <CategoryClassifier {...args} />
+    </div>
+  );
 };
+
+const Controls = styled.div`
+  padding: 1rem;
+
+  display: flex;
+  column-gap: 1rem;
+`;
+
+const TestButton = styled.button`
+  padding: 0.5rem;
+  border: 1px solid black;
+  border-radius: 8px;
+`;
 
 export const Category = Template.bind({});
 Category.args = {};
