401360c105fe73c00fe185fcb82b2b022d227312
pyo-sh <pygosky@gmail.com>
2023-06-23 17:11:46 +0900

    feat: CategoryClassifier Storybook에서 분류 테스팅 (#117)

--- src/stories/sidebar/CategoryClassifier.stories.tsx
+++ src/stories/sidebar/CategoryClassifier.stories.tsx
@@ -2,8 +2,10 @@ import styled from '@emotion/styled';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import CategoryClassifier from '@/components/sidebar/CategoryClassifier';
-import { useCategoryCreate } from '@/hooks/rq/category';
+import { useCategoryCreate, useCategoryQuery } from '@/hooks/rq/category';
 import { useCreatePlanMutation } from '@/hooks/rq/plan';
+import useClassifiedPlans from '@/hooks/useClassifiedPlans';
+import useDateState from '@/stores/date';
 import {
   createCategoryAPIHandler,
   getCategoryAPIHandler,
@@ -11,7 +13,8 @@ import {
 } from '@/stories/apis/category';
 import categoryStubManager from '@/stories/apis/data/category';
 import planStubManager from '@/stories/apis/data/plan';
-import { createPlanApiHandler } from '@/stories/apis/plan';
+import { createPlanApiHandler, getPlansApiHandler } from '@/stories/apis/plan';
+import { TIMETABLE_SCROLL_STYLE } from '@/styles/timetable';
 
 export default {
   title: 'Sidebar/Classifier',
@@ -19,6 +22,8 @@ export default {
 } as ComponentMeta<typeof CategoryClassifier>;
 
 const Template: ComponentStory<typeof CategoryClassifier> = (args) => {
+  const { year, month } = useDateState();
+  const plans = useClassifiedPlans();
   const { mutateAsync: createCategoryMutateAsync } = useCategoryCreate();
   const { mutateAsync: createPlanMutateAsync } = useCreatePlanMutation();
 
@@ -30,28 +35,95 @@ const Template: ComponentStory<typeof CategoryClassifier> = (args) => {
     const maxId = categoryStubManager.getId();
     if (maxId === 0) return;
 
-    const randomId = Math.round(Math.random() * maxId);
+    const randomId = Math.round(Math.random() * (maxId - 1)) + 1;
     createPlanMutateAsync(planStubManager.createStub({ categoryId: randomId }));
   };
 
+  const { data } = useCategoryQuery();
+  const categories = data ?? [];
+  const categoryNames = categories.reduce(
+    (names, { id, name }) => names.set(id, name),
+    new Map<number, string>(),
+  );
+  const initialCategoryPlans = categories.reduce(
+    (object, { name }) => ({ ...object, [name]: 0 }),
+    {} as { [key: string]: number },
+  );
+  const categoryPlans = plans.reduce((result, plan) => {
+    const categoryName = categoryNames.get(plan?.categoryId ?? 0);
+
+    if (categoryName) {
+      result[categoryName]++;
+    }
+
+    return result;
+  }, initialCategoryPlans);
+
   return (
-    <div>
-      <Controls>
+    <Container>
+      <div className="category-classifier-plans">
+        <h4>
+          {year}년 {month}월 일정 렌더링
+        </h4>
+        <PlanSummary>
+          <div>총 {plans.length} 개의 일정</div>
+          {Object.entries(categoryPlans).map(([name, count]) => {
+            return (
+              <div>
+                {name}({count}개)
+              </div>
+            );
+          })}
+        </PlanSummary>
+      </div>
+      <div className="category-classifier-controls">
         <TestButton onClick={createCategory}>카테고리 추가하기</TestButton>
         <TestButton onClick={createPlan}>
           랜덤 카테고리 일정 생성하기
         </TestButton>
-      </Controls>
-      <CategoryClassifier {...args} />
-    </div>
+      </div>
+      <div className="category-classifier-main">
+        <CategoryClassifier {...args} />
+      </div>
+    </Container>
   );
 };
 
-const Controls = styled.div`
-  padding: 1rem;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
 
   display: flex;
+  flex-direction: column;
+
+  .category-classifier-controls {
+    flex: 0;
+    padding: 1rem;
+
+    display: flex;
+    column-gap: 1rem;
+  }
+
+  .category-classifier-plans {
+    width: 100%;
+    flex: 0;
+    padding: 0 1rem;
+  }
+
+  .category-classifier-main {
+    flex: 1 0 0;
+  }
+`;
+
+const PlanSummary = styled.div`
+  ${TIMETABLE_SCROLL_STYLE}
+
+  padding-top: 1rem;
+  display: flex;
   column-gap: 1rem;
+
+  overflow-x: scroll;
+  overflow-y: hidden;
 `;
 
 const TestButton = styled.button`
@@ -69,6 +141,7 @@ Category.parameters = {
       createCategoryAPIHandler,
       updateCategoryAPIHandler,
       createPlanApiHandler,
+      getPlansApiHandler,
     ],
   },
 };
