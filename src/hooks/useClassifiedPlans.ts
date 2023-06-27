c8a659c7e9e6510d21637837b839dd68f0a1c0d6
pyo-sh <pygosky@gmail.com>
2023-06-27 14:38:40 +0900

    fix: useClassifiedPlans 에서 모든 tag가 hiddenTags에 포함되면 안들어가도록 설정 (#117)

--- src/hooks/useClassifiedPlans.ts
+++ src/hooks/useClassifiedPlans.ts
@@ -34,7 +34,7 @@ const useClassifiedPlans = () => {
     const { categoryId, tags } = plan;
 
     if (categoryId && hiddenCategories.has(categoryId)) return result;
-    if (tags.some((tag) => hiddenTags.has(tag))) return result;
+    if (tags.every((tag) => hiddenTags.has(tag))) return result;
     if (!showAlarm && plan.type === 'alarm') return result;
     if (!showEvent && plan.type === 'event') return result;
     if (!showTask && plan.type === 'task') return result;
