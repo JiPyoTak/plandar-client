f1bfa67fb674b162d667e9f4c999160ecf188321
seokho10007 <sukho1007@naver.com>
2023-07-13 16:38:26 +0900

    feat: 기존 분리된 컴포넌트를 하나의 컴포넌트로 병합 (#133) Co-authored-by: pyo-sh <pygosky@gmail.com>

--- src/components/sidebar/content/index.tsx
+++ src/components/sidebar/content/index.tsx
@@ -1,43 +1,54 @@
 import React from 'react';
 
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import CategoryClassifier from './CategoryClassifier';
 import SmallCalendar from './SmallCalendar';
 import TagClassifier from './TagClassifier';
-import HamburgerButton from '@/components/buttons/HamburgerButton';
+import ChevronIcon from '@/components/icons/ChevronIcon';
 import Logo from '@/components/logo';
-import useDrawerState from '@/stores/drawer';
+import UserInfo from '@/components/sidebar/content/UserInfo';
+import { MenuBorder, MenuIconWrapper } from '@/components/sidebar/styles';
+import useMenuState from '@/stores/menu';
 
 const WIDTH = '21.25rem';
 
 const SidebarContent: React.FC = () => {
-  const { isOpened } = useDrawerState();
+  const { isOpened, closeMenu } = useMenuState();
+  const theme = useTheme();
 
   return (
     <Container css={{ width: isOpened ? WIDTH : 0 }}>
       <InnerContainer>
-        <div className="sidebar-header">
-          <Logo />
-          <HamburgerButton />
-        </div>
-        <div className="sidebar-contents">
+        <Header>
+          <Logo showPicture={false} css={{ color: theme.white }} />
+          <MenuIconWrapper onClick={closeMenu}>
+            <ChevronIcon
+              type="left"
+              width={24}
+              height={24}
+              color={theme.white}
+            />
+          </MenuIconWrapper>
+        </Header>
+        <Content>
           <SmallCalendarSizer>
             <SmallCalendar />
           </SmallCalendarSizer>
-          {/*
-            Type 구현이 완료된 후 사용 할 것
-            <TypeClassifier />
-          */}
+          {/* <TypeClassifier /> */}
           <CategoryClassifier />
           <TagClassifier />
-        </div>
+        </Content>
+        <UserInfo />
       </InnerContainer>
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: relative;
+
   height: 100%;
   overflow: hidden;
   transition: width 0.3s ease-in-out;
@@ -46,32 +57,38 @@ const Container = styled.div`
 const InnerContainer = styled.div`
   width: ${WIDTH};
   min-height: 100%;
-  background-color: ${({ theme }) => theme.background3};
 
-  & > .sidebar-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
 
-    height: 4rem;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    padding: 0.5rem 1rem;
+  height: 4rem;
 
+  padding: 0.5rem 1rem;
+
+  ${MenuBorder}
+`;
+
+const Content = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  gap: 1rem;
+
+  padding: 1rem;
+
+  ${MenuBorder}
+
+  & > div {
+    border-radius: 10px;
     background-color: ${({ theme }) => theme.background1};
-  }
-
-  & > .sidebar-contents {
-    display: flex;
-    flex-direction: column;
-
-    gap: 1rem;
-
-    padding: 1rem;
-
-    & > div {
-      border-radius: 10px;
-      background-color: ${({ theme }) => theme.background1};
-    }
   }
 `;
 
