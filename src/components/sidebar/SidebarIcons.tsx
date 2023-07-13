ab076610094e6a003daf50f018c3a674e8414292
seokho10007 <sukho1007@naver.com>
2023-07-13 16:37:23 +0900

    feat: MenuIcon Click Menu 열림/닫힘 여부 설정 (#133) Co-authored-by: pyo-sh <pygosky@gmail.com>

--- src/components/sidebar/SidebarIcons.tsx
+++ src/components/sidebar/SidebarIcons.tsx
@@ -11,25 +11,56 @@ import {
   OnlyLogoIcon,
   TagIcon,
 } from '@/components/icons';
+import {
+  MenuBorder,
+  MenuIconWrapper,
+  MenuSelectedIcon,
+} from '@/components/sidebar/styles';
+import useMenuState, { TMenuState } from '@/stores/menu';
 
-const ICON_COMPONENTS = [HomeIcon, CalendarIcon, CategoryIcon, TagIcon];
+const ICON_COMPONENTS = {
+  home: HomeIcon,
+  calendar: CalendarIcon,
+  category: CategoryIcon,
+  tag: TagIcon,
+};
 
 const SidebarIcons = () => {
   const theme = useTheme();
+  const { selected, selectMenu } = useMenuState((state) => ({
+    selected: state.selected,
+    selectMenu: state.selectMenu,
+  }));
+
+  const onClickMenuIcon: React.MouseEventHandler = (e) => {
+    const target = e.target as HTMLButtonElement;
+
+    const button = target.closest('button');
+
+    if (!button) return;
+
+    const name = button.name as TMenuState;
+
+    selectMenu(name);
+  };
 
   return (
     <Container>
       <IconContainer>
         <OnlyLogoIcon width={40} height={40} color={theme.white} />
       </IconContainer>
-      <Menu>
-        {ICON_COMPONENTS.map((IconComponent, index) => (
-          <MenuIconContainer>
-            <IconComponent key={index} width={32} height={32} />
-          </MenuIconContainer>
+      <Menu onClick={onClickMenuIcon}>
+        {Object.entries(ICON_COMPONENTS).map(([key, IconComponent]) => (
+          <MenuIconWrapper
+            key={key}
+            name={key}
+            css={key === selected && MenuSelectedIcon({ theme })}
+          >
+            <IconComponent width={32} height={32} />
+          </MenuIconWrapper>
         ))}
       </Menu>
-      <IconContainer>
+      <IconContainer css={{ position: 'absolute', bottom: 0 }}>
         <ProfileImage width={40} height={40} />
       </IconContainer>
     </Container>
@@ -42,19 +73,18 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  & > div:not(:last-child) {
-    border-bottom: 1px solid ${({ theme }) => theme.border1};
-  }
+  ${MenuBorder}
 `;
 
 const IconContainer = styled.div`
-  flex: 0;
   height: 4rem;
   padding: 0.75rem;
 
   display: flex;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.primary};
 `;
 
 const Menu = styled.div`
@@ -65,29 +95,6 @@ const Menu = styled.div`
   align-items: center;
 
   row-gap: 0.5rem;
-`;
-
-const MenuIconContainer = styled.div`
-  padding: 0.25rem;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  border-radius: 5px;
-
-  & > svg * {
-    color: ${({ theme }) => theme.white};
-    stroke: ${({ theme }) => theme.white};
-  }
-
-  &:hover {
-    background-color: ${({ theme }) => theme.white};
-    & > svg * {
-      color: ${({ theme }) => theme.primary};
-      stroke: ${({ theme }) => theme.primary};
-    }
-  }
 `;
 
 export default SidebarIcons;
