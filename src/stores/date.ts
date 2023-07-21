ff757001c0ecead09937450939fd708aeca06bf2
pyo-sh <pygosky@gmail.com>
2023-07-21 13:50:00 +0900

    refactor: Date Store Type을 State, Action으로 분리 (#144)

--- src/stores/date.ts
+++ src/stores/date.ts
@@ -3,16 +3,18 @@ import { create } from 'zustand';
 
 import { decreaseMonth, increaseMonth } from '@/utils/calendar/monthHandler';
 
-type TDateYMD = Pick<IDateStore, 'day' | 'month' | 'year'>;
-
-interface IDateStore {
+type TDateState = {
   year: number;
   month: number;
   day: number;
+};
+type TDateYMD = Pick<TDateState, 'day' | 'month' | 'year'>;
+
+type TDateAction = {
   increaseStoreMonth: () => void;
   decreaseStoreMonth: () => void;
   onChangeStoreDate: (props: TDateYMD) => void;
-}
+};
 
 const getInitialDate = () => {
   const today = moment();
@@ -28,12 +30,12 @@ const initialState = {
   ...getInitialDate(),
 } as const;
 
-const useDateState = create<IDateStore>((set) => ({
+const useDateState = create<TDateState & TDateAction>((set) => ({
   ...initialState,
   increaseStoreMonth: () => set(increaseMonth),
   decreaseStoreMonth: () => set(decreaseMonth),
   onChangeStoreDate: (props: TDateYMD) => set(props),
 }));
 
-export type { IDateStore, TDateYMD };
+export type { TDateYMD };
 export default useDateState;
