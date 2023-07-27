3543560f881727521cffce343dd170e7e9bf8c1e
seokho10007 <sukho1007@naver.com>
2023-07-27 16:54:29 +0900

    feat: Popup생성시 스크롤을 막기위한 Background 생성 (#137)

---
+++ src/components/modal/plan/create/Background.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { css } from '@emotion/react';
+
+interface IProps {
+  isOpen: boolean;
+  onClose?: () => void;
+}
+
+const Background: React.FC<IProps> = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      onMouseDown={onClose}
+      css={css`
+        z-index: 20;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+      `}
+    />
+  );
+};
+
+export default Background;
