0e526a61185d4d2ed288dc9aaf792b221dc0fc36
pyo-sh <pygosky@gmail.com>
2023-07-27 14:40:52 +0900

    refactor: Range가 추가됨에 따라 Header Button 리팩토링 (#147)

--- src/components/home/main/header/HeaderButtons.tsx
+++ src/components/home/main/header/HeaderButtons.tsx
@@ -10,33 +10,14 @@ import TodayButton from '@/components/core/buttons/TodayButton';
 import useDateState from '@/stores/date';
 
 const HeaderButtons = () => {
-  const {
-    referenceDate,
-    setReferenceDate,
-    increaseReferenceDate,
-    decreaseReferenceDate,
-  } = useDateState();
-  const calendarUnit = useDateState(({ calendarUnit }) => calendarUnit);
-
-  const onClickLeftButton = () => {
-    if (calendarUnit === 'month') {
-      decreaseReferenceDate();
-    } else {
-      const decreaseTerm = calendarUnit === 'week' ? 7 : 1;
-      const newDate = moment(referenceDate).subtract(decreaseTerm, 'day');
-      setReferenceDate(newDate);
-    }
-  };
-
-  const onClickRightButton = () => {
-    if (calendarUnit === 'month') {
-      increaseReferenceDate();
-    } else {
-      const decreaseTerm = calendarUnit === 'week' ? 7 : 1;
-      const newDate = moment(referenceDate).add(decreaseTerm, 'day');
-      setReferenceDate(newDate);
-    }
-  };
+  const { setReferenceDate, increaseReferenceDate, decreaseReferenceDate } =
+    useDateState(
+      ({ setReferenceDate, increaseReferenceDate, decreaseReferenceDate }) => ({
+        setReferenceDate,
+        increaseReferenceDate,
+        decreaseReferenceDate,
+      }),
+    );
 
   const onClickTodayButton = () => {
     setReferenceDate(moment());
@@ -46,8 +27,8 @@ const HeaderButtons = () => {
     <Container>
       <PlusButton />
       <DirectionButtons
-        onClickLeftButton={onClickLeftButton}
-        onClickRightButton={onClickRightButton}
+        onClickLeftButton={decreaseReferenceDate}
+        onClickRightButton={increaseReferenceDate}
       />
       <TodayButton onClick={onClickTodayButton} />
     </Container>
