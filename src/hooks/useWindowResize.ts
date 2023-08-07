5ab94cb4f055fa0df3d0ed09b2b317d7a341372a
seokho10007 <sukho1007@naver.com>
2023-08-07 23:06:02 +0900

    feat: 화면 크기 변경시 현재 화면 크기를 저장하는 useWindowSize 훅 추가 (#156) - 디바운스를 통해 성능 최적화

---
+++ src/hooks/useWindowResize.ts
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+import useWindowSizes from '@/stores/window-size';
+
+const useWindowResize = () => {
+  const setWindowSizes = useWindowSizes((state) => state.setWindowSizes);
+
+  const debounce = (func: () => void, delay: number) => {
+    let timer: NodeJS.Timeout;
+
+    return () => {
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        func();
+      }, delay);
+    };
+  };
+
+  const onResize = () => {
+    const { innerWidth, innerHeight } = window;
+
+    setWindowSizes({ innerWidth, innerHeight });
+  };
+
+  const resizeHandler = debounce(onResize, 200);
+
+  useEffect(() => {
+    onResize();
+    window.addEventListener('resize', resizeHandler);
+
+    return () => window.removeEventListener('resize', resizeHandler);
+  }, []);
+};
+
+export default useWindowResize;
