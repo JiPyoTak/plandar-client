ced13adf8c16612ccc39fe74a41f2f7c202bdda3
seokho10007 <sukho1007@naver.com>
2023-08-23 13:48:39 +0900

    feat: 버튼 클릭시 새로운 plan을 생성하고 modal open (#165)

--- src/components/core/buttons/PlusButton.tsx
+++ src/components/core/buttons/PlusButton.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import { PlusIcon } from '@/components/common/icons';
 import StylishButton from '@/components/core/buttons/StylishButton';
+import useModalState from '@/stores/modal';
 import useFocusedPlanState from '@/stores/plan/focusedPlan';
 
 const PlusButton: React.FC = () => {
   const createNewPlan = useFocusedPlanState((store) => store.createNewPlan);
+  const openModal = useModalState((state) => state.openModal);
+
+  const onClickButton = () => {
+    createNewPlan();
+    openModal();
+  };
 
   return (
     <StylishButton
-      onClick={() => createNewPlan()}
+      onClick={onClickButton}
       size="small"
       isColor={true}
       isSquare={true}
