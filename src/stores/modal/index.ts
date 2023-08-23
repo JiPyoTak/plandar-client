f25e045bbbcd44fd3014a2abcea31f5a1797e00b
seokho10007 <sukho1007@naver.com>
2023-08-23 13:46:13 +0900

    feat: 생성 modal 창에 대한 open state 생성 (#165)

---
+++ src/stores/modal/index.ts
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+interface IModalState {
+  isOpen: boolean;
+}
+
+interface IModalAction {
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+const initialState: IModalState = {
+  isOpen: false,
+} as const;
+
+const useModalState = create<IModalState & IModalAction>((set) => ({
+  ...initialState,
+  openModal: () => {
+    set({ isOpen: true });
+  },
+  closeModal: () => {
+    set({ isOpen: false });
+  },
+}));
+
+export default useModalState;
